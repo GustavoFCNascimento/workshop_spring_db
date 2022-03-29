1743692b3eb8c19d86501007a48955d455735de0
Gustavo Fernandes <fcn.gustavo@gmail.com>
2022-03-29 14:41:47 -0300

    Usuario ID/Erro

--- src/main/java/com/udemynl/workshopmongo/service/UserService.java
+++ src/main/java/com/udemynl/workshopmongo/service/UserService.java
@@ -1,12 +1,14 @@
 package com.udemynl.workshopmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.udemynl.workshopmongo.domain.User;
 import com.udemynl.workshopmongo.repository.UserRepository;
+import com.udemynl.workshopmongo.service.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,12 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return userRepository.findAll();
-		 
+	}
+	
+	public User findById(String id){
+		Optional<User> found = userRepository.findById(id);
+		return found.orElseThrow(() -> new ObjectNotFoundException(id));
+		
+		
 	}
 }
