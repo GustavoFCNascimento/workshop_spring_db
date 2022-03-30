40a1472f63e327d36bece8f755601132ace2ab93
Gustavo Fernandes <fcn.gustavo@gmail.com>
2022-03-30 15:52:27 -0300

    Ver posts do User

--- src/main/java/com/udemynl/workshopmongo/resources/UserResource.java
+++ src/main/java/com/udemynl/workshopmongo/resources/UserResource.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.udemynl.workshopmongo.domain.Post;
 import com.udemynl.workshopmongo.domain.User;
 import com.udemynl.workshopmongo.dto.UserDTO;
 import com.udemynl.workshopmongo.service.UserService;
@@ -58,7 +59,12 @@ public class UserResource {
 		service.update(id, obj);
 		return ResponseEntity.noContent().build();
 	}
-	
+	   
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
+	}
 	
 	
 	
