0c8e131fbe87a469fa8951a5c78e23c890be04d6
Gustavo Fernandes <fcn.gustavo@gmail.com>
2022-03-30 16:00:31 -0300

    Buscando Post por ID

---
+++ src/main/java/com/udemynl/workshopmongo/service/PostService.java
@@ -0,0 +1,23 @@
+package com.udemynl.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.udemynl.workshopmongo.domain.Post;
+import com.udemynl.workshopmongo.repository.PostRepository;
+import com.udemynl.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> found = postRepository.findById(id);
+		return found.orElseThrow(() -> new ObjectNotFoundException(id));
+		
+	}
+}
