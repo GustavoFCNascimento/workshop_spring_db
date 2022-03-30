cb33f63180065e2cbef1f8e0061c2e291f7d33b4
Gustavo Fernandes <fcn.gustavo@gmail.com>
2022-03-30 17:09:31 -0300

    Consulta com @Query

--- src/main/java/com/udemynl/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/udemynl/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.udemynl.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.udemynl.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.udemynl.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> serachTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String texte);
 }
