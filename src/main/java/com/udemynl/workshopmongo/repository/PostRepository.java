6d42f0a8d79b6eeed44e57d83c026c060a68a2f6
Gustavo Fernandes <fcn.gustavo@gmail.com>
2022-03-30 15:02:47 -0300

    Criando posts

---
+++ src/main/java/com/udemynl/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.udemynl.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.udemynl.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
